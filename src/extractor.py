c7d950bb669e680f5e64f82b8382e8a9acf238e4
Rafael Brandão <raffaelbrandao@gmail.com>
2026-04-03 22:01:00 -0300

    feat: implement PDF text extraction and cleaning functions using PyMuPDF

---
+++ src/extractor.py
@@ -0,0 +1,47 @@
+# Extração de textos de arquivos em PDF. 
+# Usando PyMuPDF.
+import fitz
+import os
+import re
+
+# Lista os arquivos em um diretório.
+def list_docs(path):
+    docs = []
+
+    if not os.path.exists(path):
+        print(f"A pasta não foi encontrada: {path}")
+        return docs
+
+    for file in os.listdir(path):
+        docs.append(os.path.join(path, file))
+
+    return sorted(docs)
+
+# Função que extrai todo texto de um arquivo.
+def extract_text(path):
+    try:
+        doc = fitz.open(path)
+        full_text = ""
+        
+        for page_number in range(len(doc)):
+            page = doc[page_number]
+            full_text += page.get_text()
+        
+        doc.close()
+        return full_text
+    
+    except Exception as e:
+        print(f"Erro ao ler o caminho do arquivo {path}: {e}")
+        return ""
+
+# Limpa o texto removendo espaços e as quebras de linhas.
+def clean_text(text):
+    # Remove múltiplos espaços e as quebras de linhas.
+    text = re.sub(r'\s+', ' ', text)
+
+    # Remove espaços no início e fim
+    text = text.strip()
+
+    return text
+
+
