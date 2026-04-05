cb95fb1b77c29739ddde1df5f40e7a79f36bd6af
Rafael Brandão <raffaelbrandao@gmail.com>
2026-04-05 20:48:29 -0300

    feat: extract abstract

--- src/extractor.py
+++ src/extractor.py
@@ -117,3 +117,79 @@ def clean_title(title):
     title = title.strip()
 
     return title
+    
+# Extrai resumo
+def extract_abstract(path):
+    try:
+        doc = pymupdf.open(path)
+
+        abstract = extract_title_by_italic_format(doc)
+
+        doc.close()
+
+        if abstract:
+            abstract = clean_abstract(abstract)
+
+        return abstract
+    
+    except Exception as e:
+        print(f"Erro ao extrair resumo: {e}")
+        return ""
+
+# Extrai resumo baseado no nome da seção
+def extract_title_by_italic_format(doc):
+    try:
+        first_page = doc[0]
+        blocks = first_page.get_text("dict")
+
+        abstract_parts = []
+        found_abstract = False
+
+        for block in blocks.get("blocks", []):
+            for line in block.get("lines", []):
+                line_text = ""
+
+                for span in line.get("spans", []):
+                    text = span.get("text", "").strip()
+
+                    if text:
+                        line_text += text + " "
+
+                line_text = line_text.strip()
+
+                if not line_text:
+                    continue
+
+                if "Resumo" in line_text or "resumo" in line_text:
+                    found_abstract = True
+                    line_text = line_text.replace("Resumo", "").replace("resumo", "").strip(".: ")
+
+                    if line_text:
+                        abstract_parts.append(line_text)
+                    continue
+
+                if found_abstract:
+                    line_lower = line_text.lower()
+                    if (line_lower.startswith(("1.introdu", "1. introdu", "I.introdu", "I. introdu")) or
+                        any(phrase in line_text.lower() for phrase in ["introdução", "Introdu", "palavras chaves", "keywords"])):
+                        return abstract_parts
+
+                    abstract_parts.append(line_text)
+
+        return abstract_parts
+
+    except Exception as e:
+        print(f"Erro ao extrair resumo: {e}")
+        return ""
+    
+# Limpa o resumo
+def clean_abstract(abstract):
+    if not abstract:
+        return ""
+
+    if abstract:
+            abstract = ' '.join(abstract)
+            abstract = ' '.join(abstract.split())
+            return abstract
+
+    return ""
